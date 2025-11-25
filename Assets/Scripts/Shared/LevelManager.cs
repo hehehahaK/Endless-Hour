d8e7e065b18f2e2252971bcf4dda40bac807237b
Keno <guysuper568@gmail.com>
2025-11-25 18:14:26 +0200

    messed up fixing it

--- Assets/Scripts/Shared/LevelManager.cs
+++ Assets/Scripts/Shared/LevelManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject CurrentCheckpoint; // so we can update the current checkpoint from within unity
+    public Transform CurrentCheckpoint; // so we can update the current checkpoint from within unity
     public Transform player;
     // Start is called before the first frame update
     void Start()
@@ -15,11 +15,7 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
-    public Transform CurrentCheckpoint;   // where the player will respawn
-    public Transform player;              // drag your Player into this slot in the Inspector
-
     public void RespawnPlayer()
     {
     player.position = CurrentCheckpoint.position;

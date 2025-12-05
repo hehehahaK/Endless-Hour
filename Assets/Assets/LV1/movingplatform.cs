0089466b11cb785c268ac77d941367bd4c540ea5
janaelhelaly <abozeideldabaa@gmail.com>
2025-12-05 14:55:51 +0200

    jana

---
+++ Assets/Assets/LV1/movingplatform.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingplatform : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float moveSpeed = 2f;
+
+    private Vector3 nextPosition;
+
+    void Start()
+    {
+        nextPosition = pointB.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
+
+        if (transform.position == nextPosition)
+        {
+            if (nextPosition == pointB.position)
+            {
+                nextPosition = pointA.position;
+            }
+            else
+            {
+                nextPosition = pointB.position;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}

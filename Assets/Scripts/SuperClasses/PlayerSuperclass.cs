c82892bf6b6b2647a6738079706d099abd352177
Keno <guysuper568@gmail.com>
2025-12-05 15:26:06 +0200

    god please be done :Sob:

--- Assets/Scripts/SuperClasses/PlayerSuperclass.cs
+++ Assets/Scripts/SuperClasses/PlayerSuperclass.cs
@@ -41,19 +41,10 @@ public class PlayerSuperclass : MonoBehaviour
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
-        rb = GetComponent<Rigidbody2D>();
     }
 
-    public virtual void Update()
+    void Update()
     {
-        if (Input.GetKey(RunKey))
-        {
-            currentSpeed = moveSpeed * 2f;
-        }
-        else
-        {
-            currentSpeed = moveSpeed;
-        }
         if (Input.GetKey(RunKey))
         {
             currentSpeed = moveSpeed * 2f;
@@ -65,15 +56,16 @@ public class PlayerSuperclass : MonoBehaviour
         if (Input.GetKeyDown(Spacebar) && grounded)
         { Jump(); }
 
+        if (!Input.GetKey(L) && !Input.GetKey(R))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
 
         if (Input.GetKey(L))
         {
             Debug.Log("going left"); // note to anyone, remove later on!!
             rb.velocity = new Vector2(-currentSpeed, rb.velocity.y);
-            rb.velocity = new Vector2(-currentSpeed, rb.velocity.y);
             //player character moves horizontally to the left along the x-axis without disrupting jump
-            if (sr != null)
-            { sr.flipX = true; }
             if (sr != null)
             { sr.flipX = true; }
         }
@@ -82,15 +74,12 @@ public class PlayerSuperclass : MonoBehaviour
         {
             Debug.Log("going right");
             rb.velocity = new Vector2(currentSpeed, rb.velocity.y);
-            rb.velocity = new Vector2(currentSpeed, rb.velocity.y);
             //player character moves horizontally to the right along the x-axis without disrupting jump
             if (sr != null)
             {
                 sr.flipX = false;
-                sr.flipX = false;
             }
         }
-
 
         if (isImmune == true)
         {
@@ -112,9 +101,8 @@ public class PlayerSuperclass : MonoBehaviour
             }
         }
         anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
-        anim.SetFloat("Height", GetComponent<Rigidbody2D>().velocity.y);
+        anim.SetFloat("Height", rb.velocity.y);
         anim.SetBool("Grounded", grounded);
-        anim.SetBool("IsRunning", Input.GetKey(RunKey));
         anim.SetBool("IsRunning", Input.GetKey(RunKey));
     }
 
@@ -124,12 +112,10 @@ public class PlayerSuperclass : MonoBehaviour
         //exactly the character is considered by Unity's engine to be standing on the ground. 
     }
 
-    protected void Jump()
+    void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
-        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
     }
-
 
     void SpriteFlicker()
     {
@@ -144,7 +130,6 @@ public class PlayerSuperclass : MonoBehaviour
         }
     }
 
-
     //Damage function, we send a DMG INT value to subtract from health. it also calls LevelManager to respawn player upon death.
     public void TakeDamage(int damage)
     {
@@ -154,7 +139,6 @@ public class PlayerSuperclass : MonoBehaviour
             if (health <= 0)
             {
                 FindObjectOfType<LevelManager>().RespawnPlayer();
-                health = maxHealth;
                 health = maxHealth;
             }
             Debug.Log("Player Health:" + health.ToString());
@@ -174,12 +158,9 @@ public class PlayerSuperclass : MonoBehaviour
         {
             health = maxHealth;
         }
-        if (health > maxHealth)
-        {
-            health = maxHealth;
-        }
         Debug.Log("Player Health:" + health.ToString());
     }
+
     public void AttackBoost(int boostAmount)
     {
         if (!isBoosted)
@@ -198,5 +179,4 @@ public class PlayerSuperclass : MonoBehaviour
         AttackDamage = normalAttackDamage;
         Debug.Log("Permanent Attack Damage Upgrade: " + AttackDamage.ToString());
     }
-    // Permanent damage upgrade (doesn't expire)
 }

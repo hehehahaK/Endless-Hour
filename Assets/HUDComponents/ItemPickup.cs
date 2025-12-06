6a10a5daa9e630ecba721767e2f9bc90eb58175c
Hassan <procliamer@gmail.com>
2025-12-06 08:34:04 -0500

    pulling so khaled can do collider thingy

---
+++ Assets/HUDComponents/ItemPickup.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemPickup : MonoBehaviour
+{
+    public Item item; // Assign your ScriptableObject here
+    public KeyCode pickupKey = KeyCode.E;
+    
+    private bool playerNearby = false;
+    public GameObject pickupPromptUI; // Optional: UI prompt showing "Press E"]
+
+    void start()
+    {
+
+            pickupPromptUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(pickupKey))
+        {
+            PickUp();
+        }
+    }
+
+    void PickUp()
+    {
+        // Find the InventoryManager and add the item
+        InventoryManager inventoryManager = FindObjectOfType<InventoryManager>();
+        
+        if (inventoryManager != null)
+        {
+            inventoryManager.AddItem(item);
+            Debug.Log("Picked up: " + item.itemName);
+            Destroy(gameObject); // Remove item from world
+        }
+        else
+        {
+            Debug.LogError("InventoryManager not found!");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+
+            if (pickupPromptUI != null)
+            {
+                pickupPromptUI.SetActive(true);
+            }
+        }
+    }   
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+            
+            if (pickupPromptUI != null)
+            {
+                pickupPromptUI.SetActive(false);
+            }
+        }
+    }
+}
